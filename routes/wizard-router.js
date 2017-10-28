f22294ac80b1b23a4713840345a9893109a0c9b8
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-10-28 09:40:08 -0700

    GET request working

--- routes/wizard-router.js
+++ routes/wizard-router.js
@@ -1,3 +1,14 @@
 'use strict';
 
 const express = require('express');
+const jsonParser = require('body-parser').json();
+const Wizard = require(__dirname + '/../models/wizard.js');
+
+const wizardRouter = module.exports = express.Router();
+
+wizardRouter.get('/wizards', (req, res, next) => {
+  Wizard.find({})
+    .then(res.send.bind(res))
+    .catch(err => next({error: err}));
+});
+
