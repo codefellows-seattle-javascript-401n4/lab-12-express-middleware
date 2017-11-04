ebdebbc757d4dcb8c95b543a4104f6d8c752fc93
Kyle Richey <richey.kyle.i@gmail.com>
2017-11-04 13:55:03 -0700

    error middleware

--- lab-kyle/server.js
+++ lab-kyle/server.js
@@ -3,20 +3,23 @@
 const express = require('express');
 const app = express();
 
-// Just setting up some place holder/ reminders.
+app.use((req, res, next) => {
+  console.log('from .use middleware');
+  next();
+});
 
 app.get('/', (req, res, next) => {
-  res.send('Hello from middleware')
-}, (req,res) => {
-  res.send('Hello World! Oh I mean, Hello from server');
+  console.log('from .get middleware');
+  next();
+}, (req, res, next) => {
+  console.log('Hello World! Oh I mean, Hello from final middleware');
+  res.send('Hello World! Oh I mean, Hello from final middleware');
+  next();
 });
 
-app.post('/', (req,res) => {
-  res.send('POST request');
-});
-
-app.put('/user', (req,res) => {
-  res.send('got a PUT request at /user');
+app.use((req, res, next)  => {
+  console.log('from .use after routes middleware');
+  next();
 });
 
 app.listen(process.env.PORT || 3000);
