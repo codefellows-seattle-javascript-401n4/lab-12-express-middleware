88c5afc4c7b46472677a88b5f18b5ceafef2f211
Kyle Richey <richey.kyle.i@gmail.com>
2018-02-12 14:54:24 -0800

    cant figure how i am setting headers after i send them...error

--- lab-kyle/index.js
+++ lab-kyle/index.js
@@ -5,6 +5,4 @@ const debug = require('debug')('http:index');
 const server = require('./lib/server');
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log('server up on ', PORT);
-});
+server.listen(PORT,() => debug(`Listening on ${PORT}`));
